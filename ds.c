f8e34fa18047958b942f9bcfee99e70d9c20bc4d
Kumar Santhanam <kumar.mcasi@gmail.com>
2014-02-13 07:55:27 +0530

    first program

---
+++ ds.c
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ds.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  Thursday 13 February 2014 07:54:44  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Kumar Santhanam (), kumar.mcasi@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+		int
+main ( int argc, char *argv[] )
+{
+		printf("data structure program\n");
+		return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
